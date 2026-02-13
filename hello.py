24a0c2f95e65196d326b145383f168b5f9718034
Shruti Patil <182844121+ShrutiPatil2354@users.noreply.github.com>
2026-02-13 16:42:42 +0530

    Design completed

--- hello.py
+++ hello.py
@@ -4,4 +4,3 @@ import pandas as pd
 print("Hello, World!")
 print("This is a simple Python script that prints 'Hello, World!' to the console.")
 print("You can run this script to see the output.")
-print("This is a great way to get started with Python programming!")
