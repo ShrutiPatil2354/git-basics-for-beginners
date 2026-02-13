3c467fc4f341a5f235d4eadf117b3c6eb899fbbc
Shruti Patil <182844121+ShrutiPatil2354@users.noreply.github.com>
2026-02-13 16:47:28 +0530

    Added some features

--- hello.py
+++ hello.py
@@ -4,3 +4,4 @@ import pandas as pd
 print("Hello, World!")
 print("This is a simple Python script that prints 'Hello, World!' to the console.")
 print("You can run this script to see the output.")
+print("The way I am learning to work with git and github is awesome!")
