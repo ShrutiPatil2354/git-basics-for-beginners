2d788414e2fab6f55496ede7c1f66fc78e3c5f75
Shruti Patil <182844121+ShrutiPatil2354@users.noreply.github.com>
2026-02-13 16:36:51 +0530

    Changed files

--- hello.py
+++ hello.py
@@ -4,3 +4,4 @@ import pandas as pd
 print("Hello, World!")
 print("This is a simple Python script that prints 'Hello, World!' to the console.")
 print("You can run this script to see the output.")
+print("This is a great way to get started with Python programming!")
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 import os
 print("Hiiiii")
+print("This is the main.py file. You can run this script to execute the main functionality of your project.")
